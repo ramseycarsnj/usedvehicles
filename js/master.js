630139d902fcac3cd4f88c5f660ba76dfc40c4d3
Joe <joethemovie@gmail.com>
2017-01-10 10:38:12 -0500

    Updated Vehicles
    Remove:
    
    - SUU2731
    - SUH0482A
    - SUH0318A
    - SUG2552A
    - SUU2747
    - SUU2705
    - SUH0430A
    
    Add:
    
    - SUH0191A
    - SUH0662A
    - SUH0716A
    - SUH0625A
    - SUH0694A

--- js/master.js
+++ js/master.js
@@ -55,20 +55,8 @@ $(document).ready(function(){
     );
 }
 
+
 vehicleArray[0] = new Vehicle(
-  'SUU2705',
-  '2014 Subaru Impreza 2.5i',
-  '2.5i with Heated Front Seats + Remote Keyless Entry',
-  'Carbide Grey',
-  '$15,914',
-  'https://pictures.dealer.com/r/ramseysubaruramseysoa/0717/feb6087b3045b0a0c3c8d513a89ccf9ax.jpg',
-  'http://www.ramseysubaru.net/certified/Subaru/2014-Subaru-Legacy-ramsey-nj-33553b220a0e0a17689cb50d089dd7b1.htm?searchDepth=1:1',
-  '2.5L H-4 Cyl',
-  '24 city/32 hwy',
-  '4',
-  'Automatic CVT',
-  '42,376');
-vehicleArray[1] = new Vehicle(
   'SUU2731',
   '2014 Subaru Legacy 2.5i',
   '2.5i with Remote Keyless entry + Bluetooth',
@@ -81,71 +69,72 @@ vehicleArray[1] = new Vehicle(
   '4',
   'Automatic CVT',
   '43,928');
-vehicleArray[2] = new Vehicle(
-  'SUH0482A',
-  '2011 Toyota RAV4 Sport',
-  'Sport with Alloy Wheels',
-  'Black',
-  '$13,499',
-  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1813/347fc7654e5cbe24c486a03ff156e337x.jpg',
-  'http://www.ramseysubaru.net/new/Toyota/2011-Toyota-RAV4-ramsey-nj-33e552310a0e0adf43fd01e9e2cf0dc1.htm?searchDepth=1:1',
-  '2.5L I-4 Cyl',
+vehicleArray[1] = new Vehicle(
+  'SUH0191A',
+  '2012 Volkswagen Tiguan SE',
+  'SE',
+  'Pepper Gray',
+  '$15,387',
+  'https://images.dealer.com/autodata/us/large_stockphoto-color/2012/USC20VWS032A0/U5U5.jpg',
+  'http://www.ramseysubaru.net/new/Volkswagen/2012-Volkswagen-Tiguan-ramsey-nj-71c11d590a0e0ae77948e5c88e86f839.htm?searchDepth=1:1',
+  '2.0L i-4 Cyl AWD',
   '21 city/27 hwy',
   '4',
-  '4-Speed Automatic 4WD',
-  '58,584');
+  'Automatic with Tiptronic',
+  '39,546');
+vehicleArray[2] = new Vehicle(
+  'SUH0662A',
+  '2015 Nissan Altima 2.5 SL',
+  '2.5 SL',
+  'Cayenne Red',
+  '$17,845',
+  'https://pictures.dealer.com/r/ramseysubaruramseysoa/0135/8a97104e7a4333165d2cdd8b7c4571cbx.jpg',
+  'http://www.ramseysubaru.net/new/Nissan/2015-Nissan-Altima-ramsey-nj-a5648dd70a0e0a6b19ca8adca293b7cf.htm?searchDepth=1:1',
+  '2.5L I-4 Cyl',
+  'mpg',
+  '4',
+  'CVT with XTRONIC',
+  '18,889');
 vehicleArray[3] = new Vehicle(
-  'SUH0430A',
-  '2015 Subaru Forester 2.5i Limited',
-  '2.5i Limited with Remote Keyless Entry',
-  'Crystal Black',
-  '$22,993',
-  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1689/fbf5c41105bc259f16f6b7b41ccf2b9ex.jpg',
-  'http://www.ramseysubaru.net/certified/Subaru/2015-Subaru-Forester-ramsey-nj-33e552980a0e0adf43fd01e902ed4107.htm?searchDepth=1:1',
-  '2.5L H-4 Cyl',
-  '24 city/32 hwy',
+  'SUH0716A',
+  '2014 BMW 428i xDrive',
+  'xDrive',
+  'Alpine White',
+  '$31,500',
+  'https://pictures.dealer.com/r/ramseysubaruramseysoa/0345/0615d8caf6574aac3523697d22cfbd62x.jpg',
+  'http://www.ramseysubaru.net/new/BMW/2014-BMW-428i+xDrive-ramsey-nj-022008890a0e0a6b0cf40ddb23f0f07d.htm?searchDepth=1:1',
+  '2.0L I-4 Cyl',
+  '22 city/33 hwy',
   '4',
-  'Automatic CVT',
-  '40,107');
+  '8-Speed Automatic',
+  '38,447');
 vehicleArray[4] = new Vehicle(
-  'SUH0318A',
-  '2013 Subaru Legacy 2.5i',
-  '2.5i with Bluetooth Connectivity',
-  'Crystal Black',
-  '$10,998',
-  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1031/3ac84948f067a44ee314bc3e5b9e7edbx.jpg',
-  'http://www.ramseysubaru.net/new/Subaru/2013-Subaru-Legacy-ramsey-nj-580e14a20a0e0ae717548d639ea4d281.htm?searchDepth=1:1',
-  '2,5L H-4 Cyl CVT AWD',
-  '21 city/28 hwy',
+  'SUH0625A',
+  '2015 Subaru WRX STi',
+  'STi 2.5',
+  'Crystal Black Silica',
+  '$26,988',
+  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1416/4822c5a9def2b7f51c143bdd5480443fx.jpg',
+  'http://www.ramseysubaru.net/certified/Subaru/2015-Subaru-null-ramsey-nj-2641453b0a0e0a175837ce586833d58f.htm?searchDepth=1:1',
+  '2.5L H-4 Cyl',
+  '17 city/23 hwy',
   '4',
-  'Automatic CVT',
-  '82,849');
+  'Close Ratio 6 Speed Manual',
+  '58,158');
 vehicleArray[5] = new Vehicle(
-  'SUG2552A',
-  '2015 Subaru Forester 2.5i Premium',
-  '2.5i Premium with Remote Keyless Entry',
-  'Venetian Red',
-  '$20,979',
-  'https://pictures.dealer.com/r/ramseysubaruramseysoa/0469/8a2c573a28392e5c260e0b56dd9853c7x.jpg',
-  'http://www.ramseysubaru.net/certified/Subaru/2015-Subaru-Forester-ramsey-nj-71e2bc380a0e0a173bd765f28a0b21ca.htm?searchDepth=1:1',
-  '2.5L H-4 Cyl AWD',
-  '24 city/32 hwy',
+  'SUH0694A',
+  '2016 Subaru WRX',
+  'Base',
+  'Crystal Black Silica',
+  '$25,212',
+  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1466/c7c8ea3c348001dfbd1f61a22adf9fadx.jpg',
+  'http://www.ramseysubaru.net/certified/Subaru/2016-Subaru-WRX-ramsey-nj-2b68e8660a0e0ae75af8e08474a90fc1.htm?searchDepth=1:1',
+  '2.0L H-4 Cyl',
+  '20 city/27 hwy',
   '4',
-  'Automatic CVT',
-  '32,612');
-vehicleArray[6] = new Vehicle(
-  'SUU2747',
-  '2014 Subaru Forester 2.5i Premium',
-  '2.5i Premium with All Weather Package',
-  'Venetian Red',
-  '$18,998',
-  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1692/deb897e4e71c7586163f504e967cddb3x.jpg',
-  'http://www.ramseysubaru.net/certified/Subaru/2014-Subaru-Forester-ramsey-nj-9b8b990f0a0e0adf4ae7cb910b7256ea.htm?searchDepth=1:1',
-  '2.5L H-4 Cyl AWD',
-  '24 city/32 hwy',
-  '4',
-  'Automatic CVT',
-  '32,207');
+  '6 Speed Manual',
+  '7,621');
+
 
 
 for (var i = 0; i < vehicleArray.length; i++) {
