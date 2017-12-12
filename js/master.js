1d9c606020b6f23ae01dbdecb42da5216c3f5569
Joe Romero <joethemovie@gmail.com>
2017-12-12 13:11:52 -0500

    added new used vehicles

--- js/master.js
+++ js/master.js
@@ -55,176 +55,124 @@ $(document).ready(function(){
 
 
 
-
 vehicleArray[0] = new Vehicle(
-  'SUH2371A',
-  '2015 Subaru<br> Forester 2.5i Premium',
-  '2.5i Premium',
-  'Dark Gray Metallic<br><br>',
-  '$19,266',
-  'https://pictures.dealer.com/r/ramseysubaruramseysoa/0167/3e10fe91e5bf102e723aa23f10500103x.jpg',
-  'http://www.ramseysubaru.net/certified/Subaru/2015-Subaru-Forester-ramsey-nj-b7fe37da0a0e0a1739fb5aa3449b81e0.htm?searchDepth=1:1',
-  '2.5L H-4 Cyl',
-  '24 city/32 hwy',
-  '4',
-  'Lineartronic CVT AWD<br>',
-  '34,500');
-vehicleArray[1] = new Vehicle(
   'SUU2939',
   '2014 Subaru<br> Forester 2.5i Touring',
   '2.5i Touring',
   'Ice Silver Metallic<br><br>',
-  '$19,899',
-  'https://pictures.dealer.com/r/ramseysubaruramseysoa/0516/a546ceb9fd9c8d339f900d6158497663x.jpg',
-  'http://www.ramseysubaru.net/certified/Subaru/2014-Subaru-Forester-ramsey-nj-cc1acd160a0e0adf53250b5d7bc07e2f.htm?searchDepth=1:1',
+  '$19,763',
+  'https://pictures.dealer.com/r/ramseysubaruramseysoa/0516/a546ceb9fd9c8d339f900d6158497663x.jpg?impolicy=resize&w=650',
+  'https://www.ramseysubaru.net/certified/Subaru/2014-Subaru-Forester-ramsey-nj-cc1acd160a0e0adf53250b5d7bc07e2f.htm?searchDepth=1:1',
   '2.5L H-4 Cyl',
-  '16 city/23 hwy',
+  '24 city/32 hwy',
   '4',
   'Lineartronic CVT AWD<br>',
   '47,277');
 
-// DONE
-
-
-vehicleArray[2] = new Vehicle(
-  'SUU2942',
-  '2015 Subaru<br> WRX STi',
-  'WRX STi',
-  ' WR Blue Pearl<br><br>',
-  '$31,255',
-  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1324/bc1530d260ce64bf4e163e525090d6cbx.jpg',
-  'http://www.ramseysubaru.net/certified/Subaru/2015-Subaru-null-ramsey-nj-cc1acbd00a0e0adf53250b5d9cef5202.htm?searchDepth=1:1',
-  '2.5L H-4 Cyl',
-  '17 city/23 hwy',
-  '4',
-  'CLOSE-RATIO 6-SPEED MANUAL AWD<br>',
-  '29,276');
-
-vehicleArray[3] = new Vehicle(
-  'SUU2959',
+vehicleArray[1] = new Vehicle(
+  'SUU3038',
   '2017 Subaru<br> Legacy 2.5i',
   '2.5i',
-  'Black<br><br>',
-  '$20,850',
-  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1615/e349d8d543c60185fb8206b5506f827dx.jpg',
-  'http://www.ramseysubaru.net/used/Subaru/2017-Subaru-Legacy-ramsey-nj-d70a21f00a0e0ae80954dfcc4fd267eb.htm?searchDepth=1:1',
-  '2.5L 4-Cylinder',
+  'Ice Silver Metallic<br><br>',
+  '$25,230',
+  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1796/a28696d580843f7da5206ce4e6615bf4x.jpg?impolicy=resize&w=650',
+  'https://www.ramseysubaru.net/certified/Subaru/2017-Subaru-Legacy-ramsey-nj-c3163b220a0e0a6b40690100b9cfdebd.htm?searchDepth=1:1',
+  '2.5L H-4 Cyl',
   '25 city/34 hwy',
   '4',
   'Lineartronic CVT AWD<br>',
-  '5,157');
-
+  '4,916');
+vehicleArray[2] = new Vehicle(
+  'SUU3085',
+  '2017 Subaru<br> Legacy 2.5i',
+  '2.5i',
+  'Crystal Black Silica<br><br>',
+  '$24,780',
+  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1898/3bc5832cd3bf1abd63d7e800c62692cbx.jpg?impolicy=resize&w=650',
+  'https://www.ramseysubaru.net/certified/Subaru/2017-Subaru-Legacy-ramsey-nj-5d1534d20a0e0ae7682674b86a42d818.htm?searchDepth=1:1',
+  '2.5L H-4 Cyl',
+  '25 city/34 hwy',
+  '4',
+  'Lineartronic CVT AWD<br>',
+  '5,419');
+vehicleArray[3] = new Vehicle(
+  'SUU3087',
+  '2017 Subaru<br> Legacy 2.5i Limited',
+  '2.5i Limited',
+  'Crystal Black Silica<br><br>',
+  '$25,869',
+  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1838/9251cb10ab2eb55a69278812dd78ab89x.jpg?impolicy=resize&w=650',
+  'https://www.ramseysubaru.net/certified/Subaru/2017-Subaru-Legacy-ramsey-nj-5d1534ab0a0e0ae7682674b8a3ec39b9.htm?searchDepth=1:1',
+  '2.5L H-4 Cyl',
+  '25 city/34 hwy',
+  '4',
+  'Lineartronic CVT AWD<br>',
+  '3,015');
+vehicleArray[3] = new Vehicle(
+  'SUU3100',
+  '2017 Subaru<br> Legacy 2.5i Limited',
+  '2.5i Limited',
+  'Crystal Black Silica<br><br>',
+  '$24,883',
+  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1473/16909045b16abf6611c020f142629cd0x.jpg?impolicy=resize&w=650',
+  'https://www.ramseysubaru.net/certified/Subaru/2017-Subaru-Legacy-ramsey-nj-ab0e383d0a0e0ae867dfbbaf1527be8b.htm?searchDepth=1:1',
+  '2.5L H-4 Cyl',
+  '25 city/34 hwy',
+  '4',
+  'Lineartronic CVT AWD<br>',
+  '4,719');
 vehicleArray[4] = new Vehicle(
-  'SUU2954',
-  '2017 Subaru<br> Outback 2.5i',
-  '2.5i',
-  'Crystal White Pearl<br><br>',
-  '$24,000',
-  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1539/6b04f60602a1185d9e88edbc48336fc1x.jpg',
-  'http://www.ramseysubaru.net/certified/Subaru/2017-Subaru-Outback-ramsey-nj-d695d7d30a0e0ae80954dfccc91aa77d.htm?searchDepth=1:1',
-  '2.5L H-4 Cyl',
-  '25 city/32 hwy',
-  '4',
+  'SUU3102',
+  '2017 Subaru<br> Impreza 2.0i Limited',
+  '2.0i Limited',
+  'Crystal Black Silica<br><br>',
+  '$23,744',
+  'https://pictures.dealer.com/r/ramseysubaruramseysoa/0197/6de252dce56f8a69117df49b7e272308x.jpg?impolicy=resize&w=650',
+  'https://www.ramseysubaru.net/certified/Subaru/2017-Subaru-Impreza-ramsey-nj-ab0e378a0a0e0ae867dfbbaf5338edff.htm?searchDepth=1:1',
+  '2.0L H-4 Cyl',
+  '28 city/37 hwy',
+  '5dr hatch',
   'Lineartronic CVT AWD<br>',
-  '7,002');
-
+  '2,944');
 vehicleArray[5] = new Vehicle(
-  'SUU2958',
-  '2017 Subaru<br> Outback 2.5i',
-  '2.5i',
-  'Black<br><br>',
-  '$30,640',
-  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1193/e209a3854861c05449f8446120a163e8x.jpg',
-  'http://www.ramseysubaru.net/used/Subaru/2017-Subaru-Outback-ramsey-nj-d70a203e0a0e0ae80954dfcc7ccb9b4d.htm?searchDepth=1:1',
+  'SUU3114',
+  '2017 Subaru<br> Legacy 2.5i Limited',
+  '2.5i Limited',
+  'Crystal Black Silica<br><br>',
+  '$26,400',
+  'https://pictures.dealer.com/r/ramseysubaruramseysoa/0319/4c3a4008b8374bf770b2e2c80abbbb9ex.jpg?impolicy=resize&w=650',
+  'https://www.ramseysubaru.net/certified/Subaru/2017-Subaru-Legacy-ramsey-nj-4e107a9d0a0e0ae7394f696307b55617.htm?searchDepth=1:1',
   '2.5L H-4 Cyl',
-  '25 city/32 hwy',
+  '25 city/34 hwy',
   '4',
   'Lineartronic CVT AWD<br>',
-  '7,672');
-
+  '2,079');
 vehicleArray[6] = new Vehicle(
-  'SUU2984',
-  '2016 Subaru<br> Legacy 2.5i',
-  '2.5i',
-  'Venetian Red Pearl<br><br>',
-  '$18,569',
-  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1443/6a2a9f15e14c239a6250a281b69a7f1dx.jpg',
-  'http://www.ramseysubaru.net/certified/Subaru/2016-Subaru-Legacy-ramsey-nj-f510e29e0a0e0adf2833deeaa97a6468.htm?searchDepth=1:1',
-  '2.5L H-4 Cyl',
-  '26 city/36 hwy',
-  '4',
+  'SUU3113',
+  '2017 Subaru<br> Impreza 2.0i Limited',
+  '2.0i Limited',
+  'Island Blue Pearl<br><br>',
+  '$27,618',
+  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1251/d92adafbdde1b88ba238e0dcaabdbdffx.jpg?impolicy=resize&w=650',
+  'https://www.ramseysubaru.net/certified/Subaru/2017-Subaru-Impreza-ramsey-nj-edcf8ccd0a0e0adf5dc318966bc05774.htm?searchDepth=1:1',
+  '2.0L H-4 Cyl',
+  '28 city/37 hwy',
+  '5dr hatch',
   'Lineartronic CVT AWD<br>',
-  '7,155');
-
+  '4,069');
 vehicleArray[7] = new Vehicle(
-  'SUU2988',
-  '2017 Subaru<br> Outback 2.5i',
-  '2.5i',
-  'Black<br><br>',
-  '$24,522',
-  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1929/2d14bb92d6632c2a157895c30ce6c31bx.jpg',
-  'http://www.ramseysubaru.net/used/Subaru/2017-Subaru-Outback-ramsey-nj-f510e2db0a0e0adf2833deeaf336933b.htm?searchDepth=1:1',
-  '2.5L H-4 Cyl',
-  '25 city/32 hwy',
-  '4',
-  'Lineartronic CVT AWD<br>',
-  '5,926');
-
-vehicleArray[8] = new Vehicle(
-  'SUU2986',
-  '2017 Subaru<br> Outback 2.5i',
-  '2.5i',
-  'Crystal White Pearl  <br><br>',
-  '$24,818',
-  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1663/95c27ef3130069cea70ac435b316a0eex.jpg',
-  'http://www.ramseysubaru.net/used/Subaru/2017-Subaru-Outback-ramsey-nj-f510e22c0a0e0adf2833deeaa044daf9.htm?searchDepth=1:1',
-  '2.5L H-4 Cyl',
-  '25 city/32 hwy',
-  '4',
-  'Lineartronic CVT AWD<br>',
-  '4,996');
-
-vehicleArray[9] = new Vehicle(
-  'SUU2995',
-  '2016 Subaru<br> Crosstrek Hybrid',
-  'Hybrid',
-  'Crystal White Pearl<br><br>',
-  '$22,800',
-  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1532/600ab37f1f8d397d9d20165e6564927ex.jpg',
-  'http://www.ramseysubaru.net/used/Subaru/2016-Subaru-Crosstrek-ramsey-nj-1b59ed9e0a0e0a6b440ce99f24ec5983.htm?searchDepth=1:1',
+  'SUJ0288A',
+  '2017 Subaru<br> Impreza 2.0i Sport',
+  '2.0i Sport',
+  'Venetian Red Pearl<br><br>',
+  '$19,420',
+  'https://pictures.dealer.com/r/ramseysubaruramseysoa/0552/4a6691b1ab350b5ce5fe946ac2dd9a21x.jpg?impolicy=resize&w=650',
+  'https://www.ramseysubaru.net/certified/Subaru/2016-Subaru-Impreza-ramsey-nj-3d30fd0c0a0e0ae85da976696dc2c9ef.htm?searchDepth=1:1',
   '2.0L H-4 Cyl',
-  '30 city/34 hwy',
-  '4',
+  '28 city/37 hwy',
+  '5dr hatch',
   'Lineartronic CVT AWD<br>',
-  '12,486');
-
-vehicleArray[10] = new Vehicle(
-  'SUU2993',
-  '2016 Subaru<br> Crosstrek Hybrid',
-  'Hybrid',
-  'Crystal White Pearl<br><br>',
-  '$22,466',
-  'https://pictures.dealer.com/r/ramseysubaruramseysoa/0181/efadecf6b7d0719337912ea0d6828417x.jpg',
-  'http://www.ramseysubaru.net/used/Subaru/2016-Subaru-Crosstrek-ramsey-nj-193d88c00a0e0a6b77c3a33e8e35dcff.htm?searchDepth=1:1',
-  '2.0L H-4 Cyl',
-  '30 city/34 hwy',
-  '4',
-  'Lineartronic CVT AWD<br>',
-  '10,577');
-
-vehicleArray[11] = new Vehicle(
-  'SUU2992',
-  '2016 Subaru<br> Crosstrek Hybrid',
-  'Hybrid',
-  'Jasmine Green Pearl<br><br>',
-  '$22,986',
-  'https://pictures.dealer.com/r/ramseysubaruramseysoa/1349/fc73a295f4d86f9834ab825ae6069e72x.jpg',
-  'http://www.ramseysubaru.net/used/Subaru/2016-Subaru-Crosstrek-ramsey-nj-193d88e00a0e0a6b77c3a33eb83f0a43.htm?searchDepth=1:1',
-  '2.0L H-4 Cyl',
-  '30 city/34 hwy',
-  '4',
-  'Lineartronic CVT AWD<br>',
-  '8,486');
+  '24,954');
 
 
 for (var i = 0; i < vehicleArray.length; i++) {
